90edc7f7e4daebf387690d97ac995dec01d85ef0
Andrea Brandi <me@stari.se>
2018-06-21 21:05:03 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## Steam Saliens Game - Bot
+
+Steam Saliens Auto-play / Auto-Click script for Tempermonkey. 
+
+### Credits
+
+Thanks to meepen.
---
+++ saliensBot.user.js
@@ -0,0 +1,84 @@
+// ==UserScript==
+// @name         Steam Saliens 2018 - Bot
+// @namespace    https://github.com/starise/steam-saliens-bot
+// @version      0.0.1
+// @author       starise
+// @match        *://steamcommunity.com/saliengame/*
+// @grant        none
+// @downloadURL  https://raw.githubusercontent.com/starise/steam-saliens-bot/master/saliensBot.user.js
+// @updateURL    https://raw.githubusercontent.com/starise/steam-saliens-bot/master/saliensBot.user.js
+// ==/UserScript==
+
+(function(w) {
+	"use strict";
+
+const GAME = gGame;
+const CLICKS_PER_SECOND = 10;
+
+const InGame = function InGame() {
+    return GAME.m_State.m_EnemyManager && GAME.m_State.m_EnemyManager.m_bIsInteractive; 
+}
+
+const WORST_SCORE = -1 / 0;
+const START_POS = gApp.renderer.width;
+
+
+const EnemySpeed = function EnemySpeed(enemy) {
+    return enemy.m_Sprite.vx;
+}
+const EnemyDistance = function EnemyDistance(enemy) {
+    return (enemy.m_Sprite.x - k_nDamagePointx) / (START_POS - k_nDamagePointx);
+}
+
+
+class Attack {
+    process(enemies) {
+        throw new Error("not implemented");
+    }
+}
+
+// Basic clicking attack, attack closest
+class ClickAttack extends Attack {
+    score(enemy) {
+        if (enemy.m_bDead)
+            return WORST_SCORE;
+        return 1 - EnemyDistance(enemy);
+    }
+    process(enemies) {
+        let target, target_score = WORST_SCORE;
+
+        enemies.forEach((enemy) => {
+            if (!enemy.m_Sprite.visible)
+                return;
+            let now_score = this.score(enemy);
+            if (now_score > target_score) {
+                target = enemy, target_score = now_score;
+            }
+        });
+
+        if (target)
+            this.attack(target);
+    }
+    attack(enemy) {
+        enemy.m_Sprite.click();
+    }
+}
+
+let attacks = [
+    new ClickAttack(),
+]
+
+setInterval(function game_think() {
+    let state = GAME.m_State.m_EnemyManager;
+
+    if (!InGame()) {
+        return;
+    }
+
+    let enemies = state.m_rgEnemies;
+
+    for (let attack of attacks)
+        attack.process(enemies);
+}, 1000 / CLICKS_PER_SECOND);
+
+}(window));
