d49386db055927fe8a94e18df6503ce85a6b7f97
Andrea Brandi <me@stari.se>
2018-06-22 00:09:31 +0200

    v0.0.4

--- salienGameBot.user.js
+++ salienGameBot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         SalienGame Bot
 // @namespace    https://github.com/starise/saliengame-bot
-// @version      0.0.3
+// @version      0.0.4
 // @author       starise
 // @match        *://steamcommunity.com/saliengame/*
 // @grant        none
